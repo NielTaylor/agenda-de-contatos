69426aba9bcbae0263fa2f15fc9b4b5096022251
Carlos Rodrigues <coreytaylor4556@gmail.com>
2023-10-02 08:14:51 -0300

    iniciando o banco de dados

--- lib/helpers/contato_helper.dart
+++ lib/helpers/contato_helper.dart
@@ -1,42 +1,74 @@
-//Criando um molde para os contatos (backend)
-
 // ignore_for_file: prefer_const_declarations
-
-//1.2 instalei e importe o sqflite
 import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
 
-//1.6 abaixo vamos declarar o nome das colunas de minha tabela. Esses nomes têm
-//ques ser iguais aos nome que estar na minha classe Contato e no meu construtor
-//Contato
-//1.7 colocaremos também esses valores como "final", pois esses valores não
-//irão mudar em nenhum momento
 final String idColuna = "idColuna";
 final String nomeColuna = "nomeColuna";
 final String telefoneColuna = "telefoneColuna";
 final String emailColuna = "emailColuna";
 final String imagemColuna = "imagemColuna";
 
-class ContatoHelper {}
+//1.1 Essa classe vai conter apenas um objeto em todo nosso código. Ela não vai
+//poder ter várias instâncias ao longo do código. Por isso então vamos utilizar
+//um padrão chamado Singleton. É um padrão interessante quando queremos ter
+//apenas um objeto de nossa classe
+class ContatoHelper {
+  //1.2 primeiro declaramos "static". Ele indica que essa será uma variável que
+  //terá apenas uma em toda a minha classe (é uma variável da minha classe, e
+  //não do meu objeto)
+  //
+  //static final "Obejeto_da_classe_dentro_da_própria_classe" _instance<<muito
+  //utilizando esse nome = ContatoHelper.chamar_construtor_interno
+  //
+  //Resumindo: abaixo estou criando um objeto da minha própria classe chamado
+  //_instancia. Só terei essa única instancia do ContatoHelper. cm é um construtor
+  //interno ele só poderá ser chamado de dentro da própria classe
+  static final ContatoHelper _instancia = ContatoHelper.interno();
 
-//1.3 agora preciso definir um molde para esse contato.. como esse contato vai
-//ser criado
+  //1.4 será explicado melhor depois
+  factory ContatoHelper() => _instancia;
+
+  //1.3 Chamo ele aqui, e instancio ele no passo acima
+  ContatoHelper.interno();
+
+  //1.5 abaixo de claro o meu banco de dados..
+  //coloco underlineBanco porque eu não quero ser capaz de chamar ele de fora da
+  //minha classe
+  Database? _banco;
+
+  //1.6 agora vamos inicializar o banco de dados
+  get banco {
+    //verificamos se o banco já não estar inicializado
+    if (_banco != null) {
+      return _banco;
+      //se não estiver, iniciamos ele
+    } else {
+      _banco = iniciarBanco();
+    }
+
+    //1.7 função para inicializar o banco
+    iniciarBanco() async {
+      //1.8 primeiro, vamos pegar o local onde o banco é armazenado
+      //await pq o caminho não retornará de imediato. e async acima pq aq tem um
+      //await. o getDatabasesPath() é uma função do sqflite
+      final bancoPath = await getDatabasesPath();
+      //1.9 peguei o caminho acima, e agr irei pegar o arquivo de banco de fato
+      //o join é do pacote "path" que foi importato
+      //então o caminho do meu banco dados + o nome do arquivo retorna então
+      //o arquivo/banco em si
+      final path = join(bancoPath, "contatos.db");
+    }
+  }
+}
+
 class Contato {
-  //1.4 o id é o atributo que irá identificar o meu contato (pois pode ter
-  //contatos com nomes iguais etc.)
   int? id;
   String? nome;
   String? telefone;
   String? email;
   String? imagem;
 
-  //1.5 Vamos criar o construtor abaixo em formato de Map pois quando formos
-  //armazenar os nossos dados no banco de dados vamos armazenar em formato de
-  //mapa. E quando formos recuperar os nossos dados vamos transformar esse mapa
-  //no formato da nossa classe Contato de fato (onde cada dado é um atributo).
-  //Abaixo então já vamos fazer essa transformação
   Contato.deUmMapa(Map mapa) {
-    //1.8 agora então vou pegar os valores que chegaram aqui como mapa, e passar
-    //eles para os atributos da minha classe Contato
     id = mapa[idColuna];
     nome = mapa[nomeColuna];
     telefone = mapa[telefoneColuna];
@@ -44,38 +76,19 @@ class Contato {
     imagem = mapa[imagemColuna];
   }
 
-  //1.9 vamos ter que também transformar os dados de nosso contato (classe
-  //Contato) em um mapa. Acima pegamos o mapa e passamos para a classe Contato,
-  //agora teremos que pegar a classe contato e passar para um mapa (para poder
-  //enviar para o banco de dados/armazenar)
-
-  //função que retorna um mapa
   Map paraMapa() {
-    //2.1 string irá armazenar o campo, dynamic irá armazenar o dado em si
-    //o que fiz basicamente foi o reverso mesmo, peguei o atributo e passei
-    //para o mapa
     Map<String, dynamic> map = {
       nomeColuna: nome,
       telefoneColuna: telefone,
       emailColuna: email,
       imagemColuna: imagem,
     };
-    //2.2 Acima não iremos colocar o id, porque quem dará o id será o BD em si
-    //então quando formos enviar o dado para o BD, ele ainda não terá um id
-    //Mas vai acontecer de irmos editar ou trabalhar com os dados de um contato
-    //já salvo. Para esses momentos, vamos fazer uma checagem do id, se ele
-    //existe ou não
     if (id != null) {
-      //2.3 então se ele existir, passarei colocarei no meu mapa, que já criei
-      //acima, o valor de idColuna
       map[idColuna] = id;
     }
-    //a função então retornará esse nosso mapa
     return map;
   }
 
-  //para deixar tudo melhor então, vou dar um override no toSring, que vai fazer
-  //com que sempre que eu der um print no contato, ele me retorne essa string
   @override
   String toString() {
     return "Contato(id: $id, nome: $nome, telefone: $telefone, email: $email, imagem: $imagem)";
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,5 @@
-//Commit: Criando um molde para os contatos (backend)
-
 import 'package:flutter/material.dart';
 
 void main() {
-  //1.1 vamos primeiro criar toda a parte responsável por lidar com os dados
-  //Vamos usar o SQLite
   runApp(const MaterialApp());
 }
